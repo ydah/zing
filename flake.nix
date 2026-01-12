5d0e50428a44577c8623341f157966474bed7357
ydah <t.yudai92@gmail.com>
2026-01-12 10:56:22 +0900

    Refactor CI workflow to use Nix for Zig installation and add flake support

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "zing development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    zig = {
+      url = "github:mitchellh/zig-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+        flake-compat.follows = "flake-compat";
+      };
+    };
+    flake-compat.url = "github:edolstra/flake-compat";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, zig }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ zig.overlays.default ];
+        };
+        zigPkg = zig.packages.${system}."0.15.2";
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ zigPkg ];
+        };
+      }
+    );
+}
